de1e909df9c09b9612376e4444abcdfc999c1add
peterennis <peterennis@yahoo.com>
2020-03-10 14:49:46 -0700

    chore(): add sample todos and log output for testing

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,13 +1,21 @@
-import { Component, h, State } from '@stencil/core';
-import { Todo } from '../../interfaces/todo';
+import { Component, h, State } from "@stencil/core";
+import { Todo } from "../../interfaces/todo";
 
 @Component({
-  tag: 'app-home',
-  styleUrl: 'app-home.css'
+  tag: "app-home",
+  styleUrl: "app-home.css"
 })
 export class AppHome {
-
   @State() todos: Todo[] = [];
+
+  componentWillLoad() {
+    this.todos = [
+      { id: "1", title: "Doit One", description: "First todo description" },
+      { id: "2", title: "Doit two", description: "Second todo description" },
+      { id: "3", title: "Doit three", description: "Third todo description" }
+    ];
+    console.log(this.todos);
+  }
 
   render() {
     return [
@@ -22,10 +30,13 @@ export class AppHome {
           Welcome to the PWA Toolkit. You can use this starter to build entire
           apps with web components using Stencil and ionic/core! Check out the
           README for everything that comes in this starter out of the box and
-          check out our docs on <a href="https://stenciljs.com">stenciljs.com</a> to get started.
+          check out our docs on{" "}
+          <a href="https://stenciljs.com">stenciljs.com</a> to get started.
         </p>
 
-        <ion-button href="/profile/ionic" expand="block">Profile page</ion-button>
+        <ion-button href="/profile/ionic" expand="block">
+          Profile page
+        </ion-button>
       </ion-content>
     ];
   }
